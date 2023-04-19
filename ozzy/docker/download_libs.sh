b591e29559d72efaed0c4f4c01192d5680ee48bc
ozzyou <ozzy.ulger@gmail.com>
2023-04-19 15:25:08 +0200

    Added library download script

---
+++ ozzy/docker/download_libs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://surfdrive.surf.nl/files/index.php/s/qKY142IZ2vd1Kzp/download
+wget https://surfdrive.surf.nl/files/index.php/s/tWOKpToagVvXbwm/download
+mv download nccl-repo-ubuntu1804-2.4.8-ga-cuda10.1_1-1_amd64.deb
+mv download.1 cudnn-10.1-linux-x64-v7.6.5.32.tgz
